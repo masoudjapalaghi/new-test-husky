d0e418ee26757d538f2a05d7e355d3234ae1d1f0
masoud <masoudteymuri74@gmail.com>
2024-11-02 12:48:16 +0330

    first commit

---
+++ my-app/pages/_app.tsx
@@ -0,0 +1,7 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  let a = "test"
+  return <Component {...pageProps} />;
+}
